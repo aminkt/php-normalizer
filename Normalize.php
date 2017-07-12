36e30aedd8a4d82078e4d19be9072a6742bdd78c
Amin <ak_1596@yahoo.com>
2017-07-12 17:26:31 +0430

    Changes:
    Create Normalizer.

--- Normalize.php
+++ Normalize.php
@@ -7,5 +7,18 @@ namespace aminkt\normalizer;
  */
 class Normalize extends \Normalizer
 {
+    const STRATEGY_BY_ZERO = 1;
+    const STRATEGY_BY_COUNTRY_CODER = 2;
 
+    /**
+     * Normalize mobile number.
+     *
+     * @param string    $mobile         Mobile number.
+     * @param integer   $strategy       Strategy of normalization.
+     *
+     * @return string Mobile normalized
+     */
+    public static function normalizeMobile($mobile, $strategy=self::STRATEGY_BY_COUNTRY_CODER){
+
+    }
 }
