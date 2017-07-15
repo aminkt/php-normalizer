79eb04ba7e84fb7244f7b8a8f6b62b1550eeffb8
Amin <ak_1596@yahoo.com>
2017-07-15 17:28:15 +0430

    Changes:
    Create Normalizer.

--- Normalize.php
+++ Normalize.php
@@ -8,17 +8,33 @@ namespace aminkt\normalizer;
 class Normalize extends \Normalizer
 {
     const STRATEGY_BY_ZERO = 1;
-    const STRATEGY_BY_COUNTRY_CODER = 2;
+    const STRATEGY_BY_COUNTRY_CODE = 2;
 
     /**
      * Normalize mobile number.
      *
-     * @param string    $mobile         Mobile number.
-     * @param integer   $strategy       Strategy of normalization.
+     * @param string $mobile Mobile number.
+     * @param integer $strategy Strategy of normalization.
+     * @param string|null $countryCode Use as country code if not setted by default number.
      *
-     * @return string Mobile normalized
+     * @return bool|string Mobile normalized or false if can't normalize mobile.
      */
-    public static function normalizeMobile($mobile, $strategy=self::STRATEGY_BY_COUNTRY_CODER){
+    public static function normalizeMobile($mobile, $strategy = self::STRATEGY_BY_COUNTRY_CODE, $countryCode = '98')
+    {
+        if (preg_match('#(\+?98|0)(\d{10})$#is', $mobile, $matches)) {
+            $phone = $matches[2];
+            $cc = $matches[1];
 
+            if ($strategy == self::STRATEGY_BY_COUNTRY_CODE) {
+                if ($cc != 0) {
+                    return str_replace('+', '', $cc) . $phone;
+                } else {
+                    return $countryCode . $phone;
+                }
+            } elseif ($strategy == self::STRATEGY_BY_ZERO) {
+                return '0' . $phone;
+            }
+        }
+        return false;
     }
 }
